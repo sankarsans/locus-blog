040614d5872597efdbc8125626475a56a4eb4be9
sankarsans <sssankarsam@gmail.com>
2022-12-21 22:03:07 +0530

    modal scss changes

--- common/modal.php
+++ common/modal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
       <div class="modal-body">
